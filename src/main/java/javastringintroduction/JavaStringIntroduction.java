70c957a7dbd3623f4a6c0e3d262ef77c874dca4d
BeneZ <bezraat@gmail.com>
2020-11-18 19:30:51 +0100

    commit

---
+++ src/main/java/javastringintroduction/JavaStringIntroduction.java
@@ -0,0 +1,20 @@
+package javastringintroduction;
+
+public class JavaStringIntroduction {
+
+    public void program(String a, String b){
+        System.out.println(a.length()+b.length());
+        if (a.compareTo(b)>0){
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+        System.out.println(a.substring(0,1).toUpperCase()+a.substring(1)+" "+b.substring(0,1).toUpperCase()+b.substring(1));
+    }
+
+
+    public static void main(String[] args) {
+        new JavaStringIntroduction().program("hello","java");
+    }
+
+}
