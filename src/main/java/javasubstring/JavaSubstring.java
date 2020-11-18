a374f49f0f9a4b1f1ffa44b93ce73d4c1cd7b02c
BeneZ <bezraat@gmail.com>
2020-11-18 19:36:13 +0100

    commit

---
+++ src/main/java/javasubstring/JavaSubstring.java
@@ -0,0 +1,12 @@
+package javasubstring;
+
+public class JavaSubstring {
+
+    public void program(String S, int start, int end) {
+        System.out.println(S.substring(start, end));
+    }
+
+    public static void main(String[] args) {
+        new JavaSubstring().program("Helloworld", 3, 7);
+    }
+}
